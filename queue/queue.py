aa5f4c68e2073cdd080e22ca14a88438770a76b7
Lawrence Kimsey <lmkimsey@gmail.com>
2020-07-14 16:47:01 -0600

    Update queue.py

--- queue/queue.py
+++ queue/queue.py
@@ -13,10 +13,7 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
-import sys
-sys.path.append('..')
 
-import 
 class Queue:
     def __init__(self):
         self.size = 0
